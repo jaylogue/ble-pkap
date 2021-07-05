d3a8b30a63aa0e7a6dedde7d0ee3017a15da0adc
Jay Logue <jay.logue@gmail.com>
2021-07-05 07:23:15 -0700

    Updated nRF5 SDK version to 17.0.2

--- setup-env.sh
+++ setup-env.sh
@@ -1,5 +1,5 @@
 :
-export NRF5_SDK_ROOT=${HOME}/tools/nRF5_SDK_17.0.0_9d13099
+export NRF5_SDK_ROOT=${HOME}/tools/nRF5_SDK_17.0.2_d674dde
 export NRF5_TOOLS_ROOT=${HOME}/tools/nRF-Command-Line-Tools
 export GNU_INSTALL_ROOT=${HOME}/tools/arm/gcc-arm-none-eabi-9-2020-q2-update/bin/
 export GNU_VERSION=7.3.1
