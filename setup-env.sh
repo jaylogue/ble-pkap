555b563e3994b1d943d57848e45650de8513b54d
Jay Logue <jay.logue@gmail.com>
2021-08-06 20:38:11 -0700

    Updated setup-env.sh to use new arm toolchain

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,7 @@
 :
 export NRF5_SDK_ROOT=${HOME}/tools/nRF5_SDK_17.0.2_d674dde
 export NRF5_TOOLS_ROOT=${HOME}/tools/nRF-Command-Line-Tools
-export GNU_INSTALL_ROOT=${HOME}/tools/arm/gcc-arm-none-eabi-9-2020-q2-update/bin/
+export GNU_INSTALL_ROOT=${HOME}/tools/arm/gcc-arm-none-eabi-10.3-2021.07/bin/
 export GNU_VERSION=7.3.1
 export PATH=${PATH}:${NRF5_TOOLS_ROOT}/nrfjprog
 
